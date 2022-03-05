9d7dac2aa945800a8d78eb66777f137e9a3b0197
Jack McKinstry <jack.mckinstry@colorado.edu>
2022-03-05 13:01:54 -0700

    first main update

---
+++ main.py
@@ -0,0 +1,14 @@
+# Created by Jack McKinstry & Michael Munhbold for HackCU 8
+
+msg = "hello hack cu"
+print(msg)
+
+apiKey = input("Paste API Key: ")
+
+apiKeySecret = input("Paste API Key secret: ")
+
+bearerToken = input("Paste Bearer Token: ")
+
+print(apiKey)
+print(apiKeySecret)
+print(bearerToken)
