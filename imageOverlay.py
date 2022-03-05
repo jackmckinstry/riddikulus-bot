513acc65c98d0bea4905927a0c8334d6a981f3b1
Michael <mimu5583@colorado.edu>
2022-03-05 14:57:46 -0700

    working image overlay

--- imageOverlay.py
+++ imageOverlay.py
@@ -7,8 +7,8 @@ x_offset = 262
 y_offset = 200
 l_img[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img
 
-cv2.namedWindow('Display image')          ## create window for display
-cv2.imshow('Display image', l_img)
-cv2.waitKey(5000)  
+#cv2.namedWindow('Display image')          ## create window for display
+#cv2.imshow('Display image', l_img)
+#cv2.waitKey(5000)  
 
 Image.fromarray(l_img).save("img_result.jpg")
