cfec6b0b17106ea31e860c84c3cbdfa05c3b64a0
Michael <mimu5583@colorado.edu>
2022-03-05 14:57:07 -0700

    working image overlay

--- imageOverlay.py
+++ imageOverlay.py
@@ -1,10 +1,14 @@
 import cv2
+from PIL import Image
 
 s_img = cv2.imread("wizardRobot.jpg")
 l_img = cv2.imread("harryPotter.png")
-x_offset=y_offset=50
+x_offset = 262
+y_offset = 200
 l_img[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img
 
 cv2.namedWindow('Display image')          ## create window for display
 cv2.imshow('Display image', l_img)
-cv2.waitKey(1000)  
+cv2.waitKey(5000)  
+
+Image.fromarray(l_img).save("img_result.jpg")
