4d958bf48e2b498f002787a55a6e1a4d89ebcd4d
Michael <mimu5583@colorado.edu>
2022-03-05 23:36:26 -0700

    customized to fit profile picture to all the different gifs

--- gitOverlay.py
+++ gitOverlay.py
@@ -3,36 +3,117 @@ from PIL import Image, ImageSequence
 def fileName(status):
     match status:
         case "accio":
-            return "accioGif.gif"
+            return "gif/accioGif.gif"
         case "alohomora":
-            return "alohomoraGif.gif"
+            return "gif/alohomoraGif.gif"
         case "avada kedavra":
-            return "avadaKedavraGif.gif"
+            return "gif/avadaKedavraGif.gif"
         case "expecto patronum":
-            return "expectoPatronumGif.gif"
+            return "gif/expectoPatronumGif.gif"
         case "expelliarmus":
-            return "expelliarmusGif.gif"
+            return "gif/expelliarmusGif.gif"
         case "lumos":
-            return "lumosGif.gif"
-        case "":
-            return "muggleGif.gif"
+            return "gif/lumosGif.gif"
+        case "muggle":
+            return "gif/muggleGif.gif"
         case "obliviate":
-            return "obliviateGif.gif"
+            return "gif/obliviateGif.gif"
         case "riddikulus":
-            return "riddikulusGif.gif"
+            return "gif/riddikulusGif.gif"
         case "sectumempra":
-            return "sectumSempraGif.gif"
+            return "gif/sectumSempraGif.gif"
         case "wingardium leviosa":
-            return "wingardiumLeviosaGif.gif"
+            return "gif/wingardiumLeviosaGif.gif"
 
-background = Image.open("wizardRobot.jpg").resize((100,100))
+def xOffSet(status):
+    match status:
+        case "accio":
+            return 130
+        case "alohomora":
+            return 0
+        case "avada kedavra":
+            return 180
+        case "expecto patronum":
+            return 250
+        case "expelliarmus":
+            return 260
+        case "lumos":
+            return 20
+        case "muggle":
+            return 0
+        case "obliviate":
+            return 120
+        case "riddikulus":
+            return 220
+        case "sectumempra":
+            return 400
+        case "wingardium leviosa":
+            return 35
 
-animated_gif = Image.open("gif/accioGif.gif")
+def yOffSet(status):
+    match status:
+        case "accio":
+            return 40
+        case "alohomora":
+            return 50
+        case "avada kedavra":
+            return 40
+        case "expecto patronum":
+            return 120
+        case "expelliarmus":
+            return 25
+        case "lumos":
+            return 110
+        case "muggle":
+            return 0
+        case "obliviate":
+            return 0
+        case "riddikulus":
+            return 90
+        case "sectumempra":
+            return 45
+        case "wingardium leviosa":
+            return 45
 
-frames = []
-for frame in ImageSequence.Iterator(animated_gif):
-    frame = frame.copy()
-    frame.paste(background, (130,40))
-    frames.append(frame)
+def reSize(status):
+    match status:
+        case "accio":
+            return 100
+        case "alohomora":
+            return 100
+        case "avada kedavra":
+            return 60
+        case "expecto patronum":
+            return 100
+        case "expelliarmus":
+            return 75
+        case "lumos":
+            return 35
+        case "muggle":
+            return 1
+        case "obliviate":
+            return 80
+        case "riddikulus":
+            return 120
+        case "sectumempra":
+            return 60
+        case "wingardium leviosa":
+            return 95
 
-frames[0].save('output.gif', save_all=True, append_images=frames[1:]) 
+def produceGif(status):
+    toOpen = fileName(status)
+    xPos = xOffSet(status)
+    yPos = yOffSet(status)
+    resizeVal = reSize(status)
+
+    background = Image.open("pfp.png").resize((resizeVal,resizeVal))
+
+    animated_gif = Image.open(toOpen)
+
+    frames = []
+    for frame in ImageSequence.Iterator(animated_gif):
+        frame = frame.copy()
+        frame.paste(background, (xPos,yPos))
+        frames.append(frame)
+
+    frames[0].save('output.gif', save_all=True, append_images=frames[1:]) 
